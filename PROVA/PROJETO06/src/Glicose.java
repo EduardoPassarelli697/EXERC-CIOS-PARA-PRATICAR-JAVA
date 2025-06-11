31cc0982f6fc66ebb4dbb8b2c2d501c761aed24a
EduardoPassarelli697 <eduardo.passarelli1712@gmail.com>
2025-06-10 22:03:40 -0300

    Add files via upload

---
+++ PROVA/PROJETO06/src/Glicose.java
@@ -0,0 +1,17 @@
+public class Glicose {
+    private double nivel;
+
+    public Glicose(double nivel) {
+        this.nivel = nivel;
+    }
+
+    public String classificar() {
+        if (nivel <= 100) {
+            return "Normal";
+        } else if (nivel <= 140) {
+            return "Elevado";
+        } else {
+            return "Diabetes";
+        }
+    }
+}
